eaca1bbfba67e97791b57d27900e146234555c5b
tezadaconnect <terrencecalzada@gmail.com>
2021-11-28 09:57:22 +0800

    Created renderCancel function to cancel timeout event

--- src/client/fe_cloutendance/src/helpers/custom_hooks/use_response.helper.js
+++ src/client/fe_cloutendance/src/helpers/custom_hooks/use_response.helper.js
@@ -3,12 +3,16 @@ import { GLOBAL_ACTION_TYPE } from "../../providers/global_provider/global.reduc
 
 const useResponseHelper = () => {
   let { globalDispatch } = useGlobalContext();
+  let timeoutFail = 0,
+    timeoutSuccess = 0;
+
   const renderFail = () => {
+    renderCancel();
     globalDispatch({
       type: GLOBAL_ACTION_TYPE.setError,
       payload: { isError: true },
     });
-    setTimeout(() => {
+    timeoutFail = setTimeout(() => {
       globalDispatch({
         type: GLOBAL_ACTION_TYPE.setError,
         payload: { isError: false },
@@ -17,17 +21,35 @@ const useResponseHelper = () => {
   };
 
   const renderSucceed = () => {
+    renderCancel();
     globalDispatch({
       type: GLOBAL_ACTION_TYPE.setSuccess,
       payload: { isSuccess: true },
     });
-    setTimeout(() => {
+    timeoutSuccess = setTimeout(() => {
       globalDispatch({
         type: GLOBAL_ACTION_TYPE.setSuccess,
         payload: { isSuccess: false },
       });
     }, 5000);
   };
+
+  const renderCancel = () => {
+    clearTimeout(timeoutFail);
+    clearTimeout(timeoutSuccess);
+    timeoutFail = 0;
+    timeoutSuccess = 0;
+
+    globalDispatch({
+      type: GLOBAL_ACTION_TYPE.setSuccess,
+      payload: { isSuccess: false },
+    });
+    globalDispatch({
+      type: GLOBAL_ACTION_TYPE.setError,
+      payload: { isError: false },
+    });
+  };
+
   return { renderFail, renderSucceed };
 };
 
